b52564ace05f9cc0c689c3ee6fb8a920a4bab2f7
Javier M. Duarte <jduarte@ucsd.edu>
2021-08-14 19:57:49 -0700

    cleanup

--- make_roc.py
+++ make_roc.py
@@ -4,92 +4,24 @@ import argparse
 from collections import OrderedDict
 
 import numpy as np
-import pandas as pd
 import torch
-import seaborn as sns
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, roc_curve, auc
 import matplotlib.pyplot as plt
+import mplhep as hep
+plt.style.use(hep.style.ROOT)
 
 from hls4ml.utils.config import config_from_pyg_model
 from hls4ml.converters import convert_from_pyg_model
+
+from tqdm import tqdm
 
 # locals
 from utils.models.interaction_network_pyg import InteractionNetwork
 from utils.data.dataset_pyg import GraphDataset
 from utils.data.fix_graph_size import fix_graph_size
-
-# stat helpers
-def true_pos(target, pred):
-    n = 0
-    for i in range(len(target)):
-       if target[i]==1 and pred[i]==1:
-           n+=1
-    return n
-
-def false_pos(target, pred):
-    n = 0
-    for i in range(len(target)):
-        if target[i]==0 and pred[i]==1:
-            n+=1
-    return n
-
-def true_neg(target, pred):
-    n=0
-    for i in range(len(target)):
-        if target[i]==0 and pred[i]==0:
-            n+=1
-    return n
-
-def false_neg(target, pred):
-    n=0
-    for i in range(len(target)):
-        if target[i]==1 and pred[i]==0:
-            n+=1
-    return n
-
-def sensitivity(tp, fn):
-    if tp==0:
-        return 0
-    else:
-        return tp/(tp+fn)
-
-def specificity(tn, fp):
-    if tn==0:
-        return 0
-    else:
-        return tn/(tn+fp)
-
-def get_sens_spec(target, pred, discriminants):
-    sensit = np.zeros(len(discriminants))
-    specif = np.zeros(len(discriminants))
-
-    for i, disc in enumerate(discriminants):
-        new_pred = np.zeros(len(pred))
-        new_pred[pred>=disc]=1
-
-        tp = true_pos(target, new_pred)
-        tn = true_neg(target, new_pred)
-        fp = false_pos(target, new_pred)
-        fn = false_neg(target, new_pred)
-        sensit[i] = sensitivity(tp, fn)
-        specif[i] = specificity(tn, fp)
-
-    return sensit, specif
+from test_model import data_wrapper, load_graphs
 
 # other helpers
-class data_wrapper(object):
-    def __init__(self, node_attr, edge_attr, edge_index, target):
-        self.x = node_attr
-        self.edge_attr = edge_attr
-        self.edge_index = edge_index.transpose(0,1)
-
-        node_attr, edge_attr, edge_index = self.x.detach().cpu().numpy(), self.edge_attr.detach().cpu().numpy(), self.edge_index.transpose(0, 1).detach().cpu().numpy().astype(np.float32)
-        node_attr, edge_attr, edge_index = np.ascontiguousarray(node_attr), np.ascontiguousarray(edge_attr), np.ascontiguousarray(edge_index)
-        self.hls_data = [node_attr, edge_attr, edge_index]
-
-        self.target = target
-        self.np_target = np.reshape(target.detach().cpu().numpy(), newshape=(target.shape[0],))
-
 def get_hls_model(torch_model, graph_dims, precision='ap_fixed<16,8>', reuse=1, output_dir=""):
     # forward_dict: defines the order in which graph-blocks are called in the model's 'forward()' method
     forward_dict = OrderedDict()
@@ -121,34 +53,6 @@ def get_hls_model(torch_model, graph_dims, precision='ap_fixed<16,8>', reuse=1, 
 
     return hls_model
 
-def load_graphs(graph_indir, graph_dims, n_graphs):
-    graph_files = np.array(os.listdir(graph_indir))
-    graph_files = np.array([os.path.join(graph_indir, graph_file)
-                            for graph_file in graph_files])
-    n_graphs_total = len(graph_files)
-    IDs = np.arange(n_graphs_total)
-    dataset = GraphDataset(graph_files=graph_files[IDs])
-
-    graphs = []
-    for data in dataset[:n_graphs]:
-        node_attr, edge_attr, edge_index, bad_graph = fix_graph_size(data.x, data.edge_attr, data.edge_index,
-                                                                     n_node_max=graph_dims['n_node'],
-                                                                     n_edge_max=graph_dims['n_edge'])
-        if not bad_graph:
-            target = data.y
-            graphs.append(data_wrapper(node_attr, edge_attr, edge_index, target))
-    print(f"n_graphs: {len(graphs)}")
-
-    print("writing test bench data for 1st graph")
-    data = graphs[0]
-    node_attr, edge_attr, edge_index = data.x.detach().cpu().numpy(), data.edge_attr.detach().cpu().numpy(), data.edge_index.transpose(
-        0, 1).detach().cpu().numpy().astype(np.int32)
-    os.makedirs('tb_data', exist_ok=True)
-    input_data = np.concatenate([node_attr.reshape(1, -1), edge_attr.reshape(1, -1), edge_index.reshape(1, -1)], axis=1)
-    np.savetxt('tb_data/input_data.dat', input_data, fmt='%f', delimiter=' ')
-
-    return graphs
-
 def parse_args():
     parser = argparse.ArgumentParser()
     add_arg = parser.add_argument
@@ -157,7 +61,6 @@ def parse_args():
     add_arg('--n-graphs', type=int, default=100)
     add_arg('--max-nodes', type=int, default=112)
     add_arg('--max-edges', type=int, default=148)
-
     add_arg('--aggregation', type=str, default='add', help='[add, mean, max, all]')
     add_arg('--n-neurons', type=str, default="40")
 
@@ -165,16 +68,22 @@ def parse_args():
 
     if args.aggregation=='all':
         args.aggregation = ['add', 'mean', 'max']
-    else: args.aggregation = [args.aggregation]
+    else: 
+        args.aggregation = [args.aggregation]
 
     if args.n_neurons == "all":
         args.n_neurons = [8,40]
-    else: args.n_neurons = [int(args.n_neurons)]
+    else: 
+        args.n_neurons = [int(args.n_neurons)]
 
     return args
 
 def main():
     args = parse_args()
+    for a in args.aggregation:
+        for nn in args.n_neurons:
+            os.makedirs(f'numbers_for_paper/{a}/source_to_target/neurons_{nn}/', exist_ok=True)
+
     with open(args.config) as file:
         config = yaml.load(file, yaml.FullLoader)
 
@@ -188,9 +97,16 @@ def main():
     }
     graphs = load_graphs(graph_indir, graph_dims, args.n_graphs)
 
-    #fp_bits = [10,12,14, 16, 18, 20]
-    fp_bits = np.arange(10, 32, 2)
-    discriminants = np.linspace(0,1,100)
+    fp_bits = np.arange(10, 20, 2)
+    precisions = []
+    for fpb in fp_bits:
+        if fpb<16:
+            fpib=6
+        else: 
+            fpib=8
+        precision = f"ap_fixed<{fpb}, {fpib}>"
+        precisions.append(precision)
+
     for a in args.aggregation:
         for nn in args.n_neurons:
 
@@ -198,61 +114,52 @@ def main():
             torch_model = InteractionNetwork(aggr=a, flow="source_to_target", hidden_size=nn)
             torch_model_dict = torch.load(config['trained_model_dir'] + f"//IN_pyg_small_{a}_source_to_target_{nn}_state_dict.pt")
             torch_model.load_state_dict(torch_model_dict)
-            torch_sensitivity = np.zeros(len(discriminants))
-            torch_specificity = np.zeros(len(discriminants))
-            for i, data in enumerate(graphs):
+            
+            target_all = []
+            torch_pred_all = []
+            for i, data in tqdm(enumerate(graphs), total=len(graphs)):
                 target = data.np_target
                 torch_pred = torch_model(data).detach().cpu().numpy()
-                torch_pred = np.reshape(torch_pred[:target.shape[0]],newshape=(target.shape[0],))  # drop dummy edges
-                tsens_i, tspec_i = get_sens_spec(target, torch_pred, discriminants)
+                torch_pred = np.reshape(torch_pred[:target.shape[0]],newshape=(target.shape[0],))  # drop dummy edges  
+                target_all.append(target)
+                torch_pred_all.append(torch_pred)
 
-                torch_sensitivity += tsens_i
-                torch_specificity += tspec_i
+            target_all = np.concatenate(target_all)
+            torch_pred_all = np.concatenate(torch_pred_all)
 
-            torch_sensitivity *= 1 / len(graphs)
-            torch_specificity *= 1 / len(graphs)
-
-            df = pd.DataFrame(columns=["sensitivity", "specificity", "precision"])
-            for fpb in fp_bits:
-                if fpb<16:
-                    fpib=6
-                else: fpib=8
-                precision = f"ap_fixed<{fpb}, {fpib}>"
+            hls_pred_all = {}
+            for precision in precisions:
+                hls_pred_all[precision] = []
                 hls_model = get_hls_model(torch_model, graph_dims, precision=precision)
 
-                hls_sensitivity = np.zeros(len(discriminants))
-                hls_specificity = np.zeros(len(discriminants))
-                for i, data in enumerate(graphs):
+                for i, data in tqdm(enumerate(graphs), total=len(graphs)):
                     target = np.reshape(data.target.detach().cpu().numpy(), newshape=(data.target.shape[0],))
 
                     # hls prediction
                     hls_pred = hls_model.predict(data.hls_data)
                     hls_pred = np.reshape(hls_pred[:target.shape[0]], newshape=(target.shape[0],)) #drop dummy edges
+                    hls_pred_all[precision].append(hls_pred)
 
-                    hsens_i, hspec_i = get_sens_spec(target, hls_pred, discriminants)
-                    hls_sensitivity += hsens_i
-                    hls_specificity += hspec_i
-
-                hls_sensitivity *= 1 / len(graphs)
-                hls_specificity *= 1 / len(graphs)
-                df_i = pd.DataFrame({"sensitivity": hls_sensitivity, "specificity": hls_specificity, "precision": [f"ap_fixed<{fpb}, {fpib}>" for i in range(len(discriminants))]})
-                df = df.append(df_i, ignore_index=True)
-
-            df["false_pos"] = 1-df["specificity"].copy()
-            df["true_pos"] = df["sensitivity"].copy()
+                hls_pred_all[precision] = np.concatenate(hls_pred_all[precision])
+            
+            fpr_torch, tpr_torch, _ = roc_curve(target_all, torch_pred_all)
+            auc_torch = auc(fpr_torch, tpr_torch)*100.
             plt.figure()
-            plt.plot(1-torch_specificity, torch_sensitivity, "r", label="torch")
-            sns.lineplot(x="false_pos", y="true_pos", hue="precision", palette="crest", data=df)
-            plt.title(f"aggr={a}, hidden neurons={nn}")
-            plt.savefig(f"numbers_for_paper/{a}/{torch_model.flow}/neurons_{nn}/ROC.jpg")
+            plt.plot(fpr_torch, tpr_torch, "r", label=f"PyTorch, AUC = {auc_torch:.1f}%")
+            fpr_hls, tpr_hls, auc_hls = {}, {}, {}
+            linestyles = ['dotted', 'dashed', 'dashdot', (0, (1, 10)), (0, (5, 10)), (0, (3, 10, 1, 10))]
+            for precision, linestyle in zip(precisions, linestyles):
+                fpr_hls[precision], tpr_hls[precision], _ = roc_curve(target_all, hls_pred_all[precision])
+                auc_hls[precision] = auc(fpr_hls[precision], tpr_hls[precision])*100.
+                precision_label = precision.replace('ap_fixed','')
+                plt.plot(fpr_hls[precision], tpr_hls[precision], label=f'{precision_label}, AUC = {auc_hls[precision]:.1f}%', linestyle=linestyle)
+            plt.legend(title=f"{a} aggregation, {nn} hidden neurons")
+            plt.semilogx()
+            plt.xlabel('False positive rate')
+            plt.ylabel('True positive rate')
+            plt.savefig(f"numbers_for_paper/{a}/{torch_model.flow}/neurons_{nn}/ROC.png")
+            plt.savefig(f"numbers_for_paper/{a}/{torch_model.flow}/neurons_{nn}/ROC.pdf")
             plt.close()
-
-            df_i = pd.DataFrame(
-                {"sensitivity": torch_sensitivity, "specificity": torch_specificity, "false_pos": 1 - torch_specificity,
-                 "true_pos": torch_sensitivity, "precision": ["torch" for i in range(len(torch_sensitivity))]})
-            df = df.append(df_i, ignore_index=True)
-            os.makedirs(f"numbers_for_paper/{a}/{torch_model.flow}/neurons_{nn}", exist_ok=True)
-            df.to_csv(f"numbers_for_paper/{a}/{torch_model.flow}/neurons_{nn}/ROC_data.csv", index=False)
 
 if __name__=="__main__":
     main()
--- utils/data/fix_graph_size.py
+++ utils/data/fix_graph_size.py
@@ -68,119 +68,37 @@ def truncate_edges(node_attr, edge_attr, edge_index, n_node_max, n_edge_max):
 
     return node_attr, edge_attr_prime, edge_index_prime
 
-# removes true nodes from the node_attr matrix, replaces their indices in the edge_index matrix with the index of the last
-# included node.
-# adds dummy edges between the last included node
-def truncate_nodes_pad_edges(node_attr, edge_attr, edge_index, n_node_max, n_edge_max):
-    node_attr_prime = node_attr[:n_node_max, :]
-
-    excluded_node_indices = [i for i in range(n_node_max, node_attr.shape[0])]
-    final_node_index = node_attr_prime.shape[0] - 1
-    for i in range(edge_index.shape[0]):
-        for j in range(edge_index.shape[1]):
-            if edge_index[i, j] in excluded_node_indices:
-                edge_index[i, j] = final_node_index
-
-    n_edge_diff = n_edge_max - edge_attr.shape[0]
-    edge_attr_appendage = torch.zeros(n_edge_diff, edge_attr.shape[1], dtype=edge_attr.dtype)
-    edge_attr_prime = torch.cat([edge_attr, edge_attr_appendage], dim=0)
-
-    edge_index_appendage = torch.zeros(n_edge_diff, 2, dtype=edge_index.dtype)
-    for i in range(n_edge_diff):
-        edge_index_appendage[i, 0] = final_node_index
-        edge_index_appendage[i, 1] = final_node_index
-    edge_index_prime = torch.cat([edge_index, edge_index_appendage], dim=0)
-
-    return node_attr_prime, edge_attr_prime, edge_index_prime
-
-# removes true nodes from the node_attr matrix, replaces their indices in the edge_index matrix with the index of the last
-# included node.
-def truncate_nodes(node_attr, edge_attr, edge_index, n_node_max, n_edge_max):
-    node_attr_prime = node_attr[:n_node_max,:]
-
-    excluded_node_indices = [i for i in range(n_node_max, node_attr.shape[0])]
-    final_node_index = node_attr_prime.shape[0]-1
-    for i in range(edge_index.shape[0]):
-        for j in range(edge_index.shape[1]):
-            if edge_index[i,j] in excluded_node_indices:
-                edge_index[i,j] = final_node_index
-
-    return node_attr_prime, edge_attr, edge_index
-
-# removes true nodes
-# if #(edges between remaining nodes) >= n_edge_max:
-#  keeps the first n_edge_max of these remaining edges
-# else:
-#  keeps all the remaining edges, pads dummy edges between last included node
-def truncate_nodes_and_edges(node_attr, edge_attr, edge_index, n_node_max, n_edge_max):
-    node_attr_prime = node_attr[:n_node_max,:]
-
-    edge_attr_prime = torch.zeros(edge_attr.shape, dtype=edge_attr.dtype)
-    edge_index_prime = torch.zeros(edge_index.shape, dtype=edge_index.dtype)
-
-    excluded_nodes = [i for i in range(n_node_max, node_attr.shape[0])]
-    idx = 0
-    for i in range(edge_index.shape[0]):
-        for j in range(edge_index.shape[1]):
-            if idx >= n_edge_max:
-                continue
-
-            if edge_index[i,j] in excluded_nodes:
-                pass
-            else:
-                edge_index_prime[idx, :] = edge_index[j, :]
-                edge_attr_prime[idx,:] = edge_attr[j,:]
-                idx += 1
-
-    return node_attr_prime, edge_attr_prime, edge_index_prime
-
-def fix_graph_size(node_attr, edge_attr, edge_index, n_node_max=112, n_edge_max=148):
+def fix_graph_size(node_attr, edge_attr, edge_index, target, n_node_max=112, n_edge_max=148):
     # node_attr: [n_node, node_dim]
     # edge_attr: [n_edge, edge_dim]
     # edge_index: [2, n_edge]
+    # first remove edges that connect out-of-bound nodes
+    mask = edge_index[:,0].lt(n_node_max) & edge_index[:,1].lt(n_node_max)
+    edge_index = edge_index[mask]
+    edge_attr = edge_attr[mask]
+    target = target[mask]
+
+    # second truncate up to n_node_max and n_edge_max
+    node_attr = node_attr[:n_node_max]
+    edge_attr = edge_attr[:n_edge_max]
+    edge_index = edge_index[:n_edge_max]
+    target = target[:n_edge_max]
 
     n_node = node_attr.shape[0]
     n_edge = edge_attr.shape[0]
-    
-    if (n_node < n_node_max):
-        if (n_edge < n_edge_max):
-            node_attr_prime, edge_attr_prime, edge_index_prime = pad_nodes_and_edges(node_attr, edge_attr, edge_index, n_node_max=n_node_max, n_edge_max=n_edge_max)
-            bad_graph = False
 
-        elif (n_edge == n_edge_max):
-            node_attr_prime, edge_attr_prime, edge_index_prime = pad_nodes(node_attr, edge_attr, edge_index, n_node_max=n_node_max, n_edge_max=n_edge_max)
-            bad_graph = False
-            
-        else: #if (n_edge > n_edge_max)
-            node_attr_prime, edge_attr_prime, edge_index_prime = pad_nodes_truncate_edges(node_attr, edge_attr, edge_index, n_node_max=n_node_max, n_edge_max=n_edge_max)
-            bad_graph = True
-        
-    elif (n_node == n_node_max):
-        if (n_edge < n_edge_max):
-            node_attr_prime, edge_attr_prime, edge_index_prime = pad_edges(node_attr, edge_attr, edge_index, n_node_max=n_node_max, n_edge_max=n_edge_max)
-            bad_graph = True
+    if (n_node < n_node_max) and (n_edge < n_edge_max):
+        node_attr_prime, edge_attr_prime, edge_index_prime = pad_nodes_and_edges(node_attr, edge_attr, edge_index, n_node_max=n_node_max, n_edge_max=n_edge_max)
+        bad_graph = False
+    elif (n_node < n_node_max) and (n_edge == n_edge_max):
+        node_attr_prime, edge_attr_prime, edge_index_prime = pad_nodes(node_attr, edge_attr, edge_index, n_node_max=n_node_max, n_edge_max=n_edge_max)
+        bad_graph = False
+    elif (n_node == n_node_max) and (n_edge < n_edge_max):
+        node_attr_prime, edge_attr_prime, edge_index_prime = pad_edges(node_attr, edge_attr, edge_index, n_node_max=n_node_max, n_edge_max=n_edge_max)
+        bad_graph = True
+    else: # (n_node == n_node_max) and (n_edge == n_edge_max)
+        node_attr_prime, edge_attr_prime, edge_index_prime = node_attr, edge_attr, edge_index
+        bad_graph = False
 
-        elif (n_edge == n_edge_max):
-            node_attr_prime, edge_attr_prime, edge_index_prime = node_attr, edge_attr, edge_index
-            bad_graph = False
-
-        else:  # if (n_edge > n_edge_max)
-            node_attr_prime, edge_attr_prime, edge_index_prime = truncate_edges(node_attr, edge_attr, edge_index, n_node_max=n_node_max, n_edge_max=n_edge_max)
-            bad_graph = True
-        
-    else: #if (n_node > n_node_max)
-        if (n_edge < n_edge_max):
-            node_attr_prime, edge_attr_prime, edge_index_prime = truncate_nodes_pad_edges(node_attr, edge_attr, edge_index, n_node_max=n_node_max, n_edge_max=n_edge_max)
-            bad_graph = True
-
-
-        elif (n_edge == n_edge_max):
-            node_attr_prime, edge_attr_prime, edge_index_prime = truncate_nodes(node_attr, edge_attr, edge_index, n_node_max=n_node_max, n_edge_max=n_edge_max)
-            bad_graph = True
-
-        else:  # if (n_edge > n_edge_max)
-            node_attr_prime, edge_attr_prime, edge_index_prime = truncate_nodes_and_edges(node_attr, edge_attr, edge_index, n_node_max=n_node_max, n_edge_max=n_edge_max)
-            bad_graph = True
-
-    return node_attr_prime, edge_attr_prime, edge_index_prime, bad_graph
+    return node_attr_prime, edge_attr_prime, edge_index_prime, target, bad_graph
 
