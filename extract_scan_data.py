333115e5fe6587251c8cc892ea068a2a3950856f
abdelabd <aelabd@sas.upenn.edu>
2021-11-11 19:36:04 -0500

    manually imported plot-changes from main branch

--- extract_scan_data.py
+++ extract_scan_data.py
@@ -15,20 +15,22 @@ def parse_vsynth_report(dir):
         file=f.read()
     file = file.split("\n")
 
-    terms_of_interest = ["CLB LUTs*", "CARRY8", "DSPs", "Bonded IOB"]
+    terms_of_interest = ["CLB LUTs*", "CARRY8", "DSPs", "Bonded IOB", "RAMB18"]
     percent_util_dict = {i: None for i in terms_of_interest}
-    lines_of_interest = []
     for line in file:
         for term, val in percent_util_dict.items():
             if (term in line) and val==None:
-                percent_util_dict[term] = line.split("|")[5]
-                lines_of_interest.append(line)
+                used = float(line.split("|")[2].strip())
+                available = float(line.split("|")[4].strip())
+                percent_util_dict[term] = 100*used/available
+
     out_dict = {k+" (%) V-Synth":v for k,v in percent_util_dict.items()}
 
     for line in file:
         if "Register as Flip Flop" in line:
-            FF_usage = line.split("|")[5]
-            out_dict["FF (%) V-Synth"] = FF_usage
+            used = float(line.split("|")[2].strip())
+            available = float(line.split("|")[4].strip())
+            out_dict["FF (%) V-Synth"] = 100*used/available
     return out_dict
 
 def parse_csynth_report(dir):
@@ -38,34 +40,64 @@ def parse_csynth_report(dir):
 
     out_dict = {}
     latency_found = False
-    util_slr_found = False
-    util_found = False
+    usage_found = False
+
     for i, line in enumerate(file):
-        if ("Latency  |  Interval" in line) and not latency_found:
-            line_of_interest = file[i+3].split("|")
-            out_dict["Latency min (clock cycles)"] = line_of_interest[1]
-            out_dict["Latency max (clock cycles)"] = line_of_interest[2]
-            out_dict["Interval min (clock cycles)"] = line_of_interest[3]
-            out_dict["Interval max (clock cycles)"] = line_of_interest[4]
-            latency_found = True
+        if not latency_found:
+            if ("Latency   |  Interval" in line) or ("Latency  |  Interval" in line):
+                line_of_interest = file[i+3].split("|")
+                out_dict["Latency min (clock cycles)"] = line_of_interest[1]
+                out_dict["Latency max (clock cycles)"] = line_of_interest[2]
+                out_dict["Interval min (clock cycles)"] = line_of_interest[3]
+                out_dict["Interval max (clock cycles)"] = line_of_interest[4]
+                latency_found = True
 
-        if ("Utilization SLR (%)" in line) and not util_slr_found:
+        if ("|Total                |" in line) and not usage_found:
             line_of_interest = line.split("|")
-            out_dict["BRAM_18K SLR (%) C-Synth"] = line_of_interest[2]
-            out_dict["DSP48E SLR (%) C-Synth"] = line_of_interest[3]
-            out_dict["FF SLR (%) C-Synth"] = line_of_interest[4]
-            out_dict["LUT SLR (%) C-Synth"] = line_of_interest[5]
-            out_dict["URAM SLR (%) C-Synth"] = line_of_interest[6]
-            util_slr_found = True
+            bram18k_used = float(line_of_interest[2].strip())
+            dsp48_used = float(line_of_interest[3].strip())
+            ff_used = float(line_of_interest[4].strip())
+            lut_used = float(line_of_interest[5].strip())
+            uram_used = float(line_of_interest[6].strip())
 
-        if ("Utilization (%)" in line) and not util_found:
-            line_of_interest = line.split("|")
-            out_dict["BRAM_18K (%) C-Synth"] = line_of_interest[2]
-            out_dict["DSP48E (%) C-Synth"] = line_of_interest[3]
-            out_dict["FF (%) C-Synth"] = line_of_interest[4]
-            out_dict["LUT (%) C-Synth"] = line_of_interest[5]
-            out_dict["URAM (%) C-Synth"] = line_of_interest[6]
-            util_found = True
+            try:
+                available_slr_line = file[i+2]
+            except IndexError:
+                continue
+
+            line_of_interest = available_slr_line.split("|")
+            bram18k_slr_avail = float(line_of_interest[2].strip())
+            dsp48_slr_avail= float(line_of_interest[3].strip())
+            ff_slr_avail = float(line_of_interest[4].strip())
+            lut_slr_avail = float(line_of_interest[5].strip())
+            uram_slr_avail = float(line_of_interest[6].strip())
+
+            out_dict["BRAM_18K SLR (%) C-Synth"] = 100*bram18k_used / bram18k_slr_avail
+            out_dict["DSP48E SLR (%) C-Synth"] = 100*dsp48_used / dsp48_slr_avail
+            out_dict["FF SLR (%) C-Synth"] = 100*ff_used / ff_slr_avail
+            out_dict["LUT SLR (%) C-Synth"] = 100*lut_used / lut_slr_avail
+            out_dict["URAM SLR (%) C-Synth"] = 100*uram_used / uram_slr_avail
+
+            try:
+                available_line = file[i+6]
+            except IndexError:
+                continue
+
+            line_of_interest = available_line.split("|")
+            bram18k_avail = float(line_of_interest[2].strip())
+            dsp48_avail = float(line_of_interest[3].strip())
+            ff_avail = float(line_of_interest[4].strip())
+            lut_avail = float(line_of_interest[5].strip())
+            uram_avail = float(line_of_interest[6].strip())
+
+            out_dict["BRAM_18K (%) C-Synth"] = 100*bram18k_used/bram18k_avail
+            out_dict["DSP48E (%) C-Synth"] = 100*dsp48_used/dsp48_avail
+            out_dict["FF (%) C-Synth"] = 100*ff_used/ff_avail
+            out_dict["LUT (%) C-Synth"] = 100*lut_used/lut_avail
+            out_dict["URAM (%) C-Synth"] = 100*uram_used/uram_avail
+
+            usage_found = True
+
     return out_dict
 
 def get_single_report(dir):
@@ -78,6 +110,7 @@ def main():
     args = parse_args()
 
     columns = ["Project", "CLB LUTs* (%) V-Synth", "CARRY8 (%) V-Synth", "DSPs (%) V-Synth", "Bonded IOB (%) V-Synth",
+               "RAM18 (%) V-Synth",
                "Latency min (clock cycles)", "Latency max (clock cycles)", "Interval min (clock cycles)",
                "Interval max (clock cycles)", "BRAM_18K SLR (%) C-Synth", "DSP48E SLR (%) C-Synth",
                "FF SLR (%) C-Synth", "LUT SLR (%) C-Synth", "URAM SLR (%) C-Synth", "BRAM_18K (%) C-Synth",
